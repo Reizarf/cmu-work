fb11a223496725f467066596f6cf8b98e31fb3a4
reizarf <sullyfrazier@gmail.com>
2021-11-03 18:48:23 -0600

    work

--- cs3/Assignment3/brute.cpp
+++ cs3/Assignment3/brute.cpp
@@ -1,60 +1,15 @@
-#include <iostream>
-#include <fstream>  
-#include <iomanip>
-using namespace std;
-int main()
+static int BruteCount= 0, BruteComp = 0;
+int BruteCountF()
 {
-    
-    // char T[1000];
-    // char P[100];
-    
-    // ifstream read;
-
-    // int markNum;
-
-    // read.open("testfile.txt");
-
-    // if(!read.good())
-    // {
-    //     cerr << "File not found" << endl;
-    //     exit(1);
-    // }
-    // read >> T;
-    
-
-    // for(int i = 0; i < 1000; i++)
-    // {
-    //     cout << T[i];
-    // }
-
-
-
-    //need to be able to read in the entire file in a few lines
-    //seekg()?????
-    //look into cout.write
-    //get the readfile function working
-    //
-    //
-
-    ifstream is("testfile.txt",ifstream::binary);
-    if(is)
-    {
-        is.seekg(0,is.end);//setting position to the start
-        int len = 0;
-        is.seekg(0,is.beg);//setting postiion to eof
-
-        char * buffer = new char[len];
-
-        is.read(buffer,len);
-
-        is.close();
-
-        cout.write(buffer,len);
-        cout << buffer;//print here
-
-        delete[] buffer;
-
-    }
-
-    return 0;
+    return BruteCount;
 }
+int BruteCompareF()
+{
+    return BruteComp;
+}
+void BruteReset()
+{
+    BruteComp = 0;
+    BruteCount = 0;
+}
+
--- cs3/Assignment3/main.cpp
+++ cs3/Assignment3/main.cpp
@@ -3,61 +3,138 @@
 #include <string.h>
 
 using namespace std;
-
-char* shiftTable(char pattern[])
+int main()
 {
-    int m = strlen(pattern);
-    static char Table[26];
+    string userInput;
+
+    int fLen;
+
+    char fileName[fLen];
+
+    cout << "Enter a filename to read: " << endl;
+    cin >> fileName;
+
+    fstream inFile;
+
+    if(strcmp(&fileName[strlen(fileName)-4], ".txt") == 0)
+    {
+        inFile.open(fileName,ios::in);
+    }
+    inFile.seekg(0,ios::end);
+    int fileLen;
+    fileLen = (int)inFile.tellg();
+    inFile.seekg(0,ios::beg);
+
+    char *bufferArray = new char[fileLen];
+
+    inFile.read(bufferArray,fileLen);
+
+    inFile.close();
+
+    cout << bufferArray << endl;
+
+    string testPiece;
+
+    cout << "enter the string you'd like to search for: "<<endl;
+    cin >> testPiece;
+    int patternSize = testPiece.length();
+    char sample[patternSize];
+
+    strcpy(sample,testPiece.c_str());
+
+
+    //cin string 
+    //strcpy the string to a character array
+    //that's what we call the function on
+
+    //now I need to call horspool
+    //bruteforce
+    //and kmp
+
+    int * results[] = horsPoolMatching(bufferArray,sample);
+
+    //createa pointer integer set it = to function
+    //create integer function with counting and num of comparisons
+
+    //create reset function^^
+
+    //in functions int * position = new int[10000];
+    //for int i = 0; i < countFunction; i++)
+    //  cout << pos[i];
+
+
+    horsPoolMatching(bufferArray,sample);
+    delete[] results;
+
+    return 0;
+
+}
+
+
+// int horsPoolCount()
+// {
+//     return horsPoolCount;
+// }
+
+
+// char* shiftTable(char pattern[])
+// {
+//     int m = strlen(pattern);
+//     static char Table[26];
     
 
-    for(int i = 0; i < strlen(pattern); i++)
-    {
-        Table[i] = m;
-    }
-    for(int j = 0; j < m-2; j++)
-    {
-        Table[pattern[j]] = m - 1 - j;
-    }
-    return Table;
+//     for(int i = 0; i < strlen(pattern); i++)
+//     {
+//         Table[i] = m;
+//     }
+//     for(int j = 0; j < m-2; j++)
+//     {
+//         Table[pattern[j]] = m - 1 - j;
+//     }
+//     return Table;
 
-}
-int horsPoolMatching(char pattern[], char data[])
-{
-    //call shift table
-    shiftTable(pattern);
+// }
 
-    //takes both pattern and text to read against
-    //output
-    //the index of the left end of the first
-    //matching substring
-    //  or -1 if there are no matches
+// int horsPoolMatching(char pattern[], char data[])
+// {
+//     //call shift table
+//     int* position = new int[1000];
+
+//     shiftTable(pattern);
+
+//     //takes both pattern and text to read against
+//     //output
+//     //the index of the left end of the first
+//     //matching substring
+//     //  or -1 if there are no matches
 
 
-    //m is strlen of pattern-1
-    //n is strlen of data-1
-    int k = 0;
-    int m = strlen(pattern -1);
-    int n = strlen(data -1);
-    int i = m - 1;
+//     //m is strlen of pattern-1
+//     //n is strlen of data-1
+//     int k = 0;
+//     int m = strlen(pattern -1);
+//     int n = strlen(data -1);
+//     int i = m - 1;
 
-    while(i <= n -1)
-    {
-        k=0;
-        while ((k <= m - 1) && (pattern[m-1-k] = data[i-k]))
-        {
-            k = k + 1;
-        }
-        if( k == m)
-        {
-            return i - m + 1;
-        }
-        else
-        {
-        i = i + data[data[i]];
-        }
-    }
-    return -1;
-}
+//     while(i <= n -1)
+//     {
+//         k=0;
+//         while ((k <= m - 1) && (pattern[m-1-k] = data[i-k]))
+//         {
+//             k = k + 1;
+//         }
+//         if( k == m)
+//         {
+//             return i - m + 1;
+//         }
+//         else
+//         {
+//         i = i + data[data[i]];
+//         }
+//     }
+//     return position;
+// }
+
 
 void computeLPSArray(char* pattern, int M, int* lps);
 
@@ -138,84 +215,12 @@ void computeLPSArray(char* pattern, int M, int* lps)//compute prefix function
     }
 }
 
-int main()
-{
-    string userInput;
-
-    int fLen;
-
-    char fileName[fLen];
-
-    cout << "Enter a filename to read: " << endl;
-    cin >> fileName;
-
-    fstream inFile;
-
-    if(strcmp(&fileName[strlen(fileName)-4], ".txt") == 0)
-    {
-        inFile.open(fileName,ios::in);
-    }
-    inFile.seekg(0,ios::end);
-    int fileLen;
-    fileLen = (int)inFile.tellg();
-    inFile.seekg(0,ios::beg);
-
-    char *bufferArray = new char[fileLen];
-
-    inFile.read(bufferArray,fileLen);
-
-    inFile.close();
-
-    cout << bufferArray << endl;
-
-    string testPiece;
-
-    cout << "enter the string you'd like to search for: "<<endl;
-    cin >> testPiece;
-    int patternSize = testPiece.length();
-    char sample[patternSize];
-
-    strcpy(sample,testPiece.c_str());
 
 
-    //cin string 
-    //strcpy the string to a character array
-    //that's what we call the function on
 
-    //now I need to call horspool
-    //bruteforce
-    //and kmp
-
-    horsPoolMatching(bufferArray,sample);
-
-    return 0;
-
-}
 
 
 
 
 
 //brute force from ben
-int bruteForce(char* tex, char* pattern){ // dont know if this is working
-    cout << tex << endl;
-    cout << pattern << endl;
-    int textLength=strlen(tex);
-    int patternLength=strlen(pattern);
-    cout << textLength << endl;
-    cout << patternLength << endl;
-    for(int i=0; i < textLength; i++){
-        cout << "here " << i << endl;
-        int j=0;
-        while(j<patternLength && pattern[j]==tex[i+j])
-        {
-            j++;
-            cout << j << endl;
-        }
-        if(j==patternLength) {
-          cout << "i= " << i<<endl;
-          return i;
-        }
-    }
-    return -1;
-}
