33e4b20eee0420e56b637d72e8c75f84cc2c6c86
reizarf <sullyfrazier@gmail.com>
2021-11-05 13:36:07 -0600

    work

--- cs3/Assignment3/main.cpp
+++ cs3/Assignment3/main.cpp
@@ -1,3 +1,17 @@
+/*
+I declare that all material in this assessment task is
+my work except where there is clear acknowledgement
+or reference to the work of others. I further declared
+that I have complied to the CMU Academic Integrity
+policy at the University Website. 
+https://coloradomesa.edu/student-services/documents
+Authors Name:       Sullivan Frazier
+UID(700#:)          700-479463
+Date:               13 October 2021
+
+Submissions that do not include the above academic 
+integrity statement will not be considered.
+*/
 #include <iostream>
 #include <fstream>
 #include <string.h>
