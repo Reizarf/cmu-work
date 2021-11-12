940b7971f98d2dc00fdc981623e07e6a583093b7
Reizarf <sullyfrazier@gmail.com>
2021-11-12 13:14:06 -0800

    work

--- python/basnetKattis/AnotherBrick/AnotherBrick.py
+++ python/basnetKattis/AnotherBrick/AnotherBrick.py
@@ -1,5 +1,5 @@
 #referenced  https://github.com/ionelh/kattis/blob/master/python3/solutions/anotherbrick.py
-
+import unittest
 import sys
 
 header = [int(x) for x in sys.stdin.readline().split()]
@@ -10,22 +10,39 @@ target_height = header[0]
 
 row_w = 0
 rows = 0
+def rows(rows):
+    rows+=1
 
 result = ''
+def brick():
+    row_w = 0
+    rows = 0
+    for brick in bricks:
+        row_w += brick
+        if row_w == target_width:
+            #rows +=1
+            rows()
+            if rows == target_height:
+                result = 'YES'
+                break
+            row_w = 0
 
-for brick in bricks:
-    row_w += brick
-
-    if row_w == target_width:
-        rows +=1
-
-        if rows == target_height:
-            result = 'YES'
+        if row_w > target_width:
+            result = 'NO'
             break
-        row_w = 0
+    print(result)
 
-    if row_w > target_width:
-        result = 'NO'
-        break
-print(result)
+class Tests(unittest.TestCase):
+    def test(self):
+        self.assertEquals(rows(rows),rows+1)
 
+# if __name__ == '__main__':
+#     unittest.main()
+
+x = '2 10 7 5 5 5 5 5'
+brick()
+
+#print(int(x),base=16)
+
+# one = list(input(x))
+#print("one",one)
