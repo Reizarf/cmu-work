fad678171250c9397d8869a8c4e94eb2ecb69e27
reizarf <sullyfrazier@gmail.com>
2021-11-27 17:22:08 -0700

    work

--- cs3/ass4/py/cardGame.py
+++ cs3/ass4/py/cardGame.py
@@ -1,11 +1,76 @@
 import random, itertools
 #generate deck
 deck = list(itertools.product(range(1,14),['Spade','Heart','Diamond','Club']))
-#shuffle
-random.shuffle(deck)
 
-#draw top card
-print("Top two cards are: ")
-for i in range(2):
-    print(deck[i][0], "of ",deck[i][1])
+cardNum = ("two","three","four","five","six","seven","eight","nine","ten",
+"jack","queen","king")
 
+suit = ("diamonds","hearts","clubs","spades")
+
+
+def randomFaceValue(cardNum):#generate random face
+    randomFace = random.choice(cardNum)
+    print("Random card is: ",randomFace)
+    return randomFace
+
+def randomSuitValue(suit):
+    randomSuit = random.choice(suit)
+    return randomSuit
+
+def randomBOTH(cardNum,suit):
+    random_card = randomFaceValue,randomSuitValue
+    return random_card
+
+
+def guessCardNum(randomFace):
+    print("randomFaceValue:",randomFace)
+    guess = str(input("Enter your guess: (ex)'six','seven'."))
+    print("Your guess was: ",guess)
+    if(guess == cardNum):
+        print("You were correct!")
+        
+    else:
+        print("Guess again")
+        
+
+
+
+  
+print('''
+    Enter your selction:
+
+    Guess only Face Value(A)
+    Guess only suit(B)
+    Guess face and suit(C)
+    Quit(Q)
+     ''')
+
+selection = input()
+
+print(selection)
+while(selection != 'Q'):
+    
+    
+
+    if(selection == 'A'):
+        randomFaceValue(cardNum)#random card number
+        print("Guessing face value")
+        #guessCardNum()
+
+        break
+        #function call
+    if(selection == 'B'):
+        randomSuitValue(suit)#random suit value
+        print("You chose to guess the suit value")
+        #function call
+
+        break
+    if(selection == 'C'):
+        #function call
+        randomBOTH(cardNum,suit)
+        print("Guess the face first then the suit")
+
+        break
+
+
+    
