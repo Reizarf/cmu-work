4351d712556d8171f9b41cd3df4c86817512def4
reizarf <sullyfrazier@gmail.com>
2021-12-02 19:34:36 -0700

    work

--- python/finalKattis/softpasswords/test.py
+++ python/finalKattis/softpasswords/test.py
@@ -3,19 +3,25 @@ from softpasswords import problem
 
 class Test_password(unittest.TestCase):
     def test1_pass(self):
-        S = "123"
-        P = "123a"
+        S = "A$AP"
+        P = "R0CKY"
         #print("func call",problem(S,P))
         problem(S,P)
         answer = 'No'
         self.assertEqual(answer,problem(S,P),"broken test case 1")
     def test2_pass(self):
-        S = "pass"
-        P = "password"
+        S = "passc0de"
+        P = "passW04D!"
         problem(S,P)
         answer = 'No'
         self.assertEqual(answer,problem(S,P),"broken")
-    #def test 3
+    def test3_pass(self):
+        S = "KATTIS"
+        P = "Pr0bl3ms!"
+        problem(S,P)
+        answer = 'No'
+        self.assertEqual(answer,problem(S,P),"broken!")
+
     
 if __name__ == "__main__":
     unittest.main(verbosity=2)
